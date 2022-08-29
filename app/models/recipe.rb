61f3eabcc4854408e3087dfd1425112fc2cb8b2d
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-08-29 21:25:39 +0500

    Add validations to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-
   has_many :recipes_foods, dependent: :destroy
+
+  validates :name, :description, :cooking_time, :preparation_time, presence: true
 end
