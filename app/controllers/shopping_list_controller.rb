bf58ac3719cc3447b4e60995d34493a278cd2d55
EidHachem <eidhachem1@gmail.com>
2022-08-30 23:25:08 +0300

    Create shopping_list controller

---
+++ app/controllers/shopping_list_controller.rb
@@ -0,0 +1,18 @@
+class ShoppingListController < ApplicationController
+  def index
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = RecipeFood.all
+    @food = Food.all
+    @total_price = current_user.sum(@recipe.recipe_food.includes([:food]))
+  end
+
+  def show
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = RecipeFood.all
+    @food = Food.all
+    @total_price = 0
+    @recipe_food.each do |fd|
+      @total_price += fd.quantity * fd.food.price
+    end
+  end
+end
