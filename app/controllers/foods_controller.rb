e44d4233325ec4691f6962fa0eb4565b736756fe
EidHachem <eidhachem1@gmail.com>
2022-08-30 14:42:21 +0300

    Add create method to foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -11,6 +11,16 @@ class FoodsController < ApplicationController
     @food = Food.new
   end
 
+  def create
+    @user = current_user
+    @food = current_user.foods.new(user_id: current_user.id, name: params[:name], price: params[:price], quantity: params[:quantity], measurement_unit: params[:measurement_unit])
+    if @food.save
+      redirect_to(user_foods_path(user_id: @user.id), notice: 'Food was successfully created.')
+    else
+      link_to('Back', user_food_path(user_id: @user.id))
+    end
+  end
+  
   def destroy
     @food = Food.find(params[:id])
     @food.delete
