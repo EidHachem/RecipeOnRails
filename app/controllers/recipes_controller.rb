7b1475e2224cbce02f0ecd502d49e98e2d75c576
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-08-30 11:43:42 +0500

    Add delete button in recipe list page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,7 @@ class RecipesController < ApplicationController
 
   def destroy
     @recipe = Recipe.find(params[:id])
-    @recipe.destroy
+    @recipe.delete
     respond_to do |format|
       format.html do
         redirect_to user_recipes_path(user_id: @recipe.user.id), notice: 'Recipe was successfully deleted.'
