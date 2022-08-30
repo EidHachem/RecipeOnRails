e597c6be76e8b819fa6b16892488cced40760e47
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-08-30 08:17:07 +0500

    Define user permissions in ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    return unless user.present?
+    can :manage, Recipe, user_id: user.id
+    can :manage, Food, user_id: user.id
+
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
