f91aca50c14fd0b9697c3df87f84d0fb6f4d659c
EidHachem <eidhachem1@gmail.com>
2022-08-30 15:13:26 +0300

    Fix linters errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -13,7 +13,8 @@ class FoodsController < ApplicationController
 
   def create
     @user = current_user
-    @food = current_user.foods.new(user_id: current_user.id, name: params[:name], price: params[:price], quantity: params[:quantity], measurement_unit: params[:measurement_unit])
+    @food = current_user.foods.new(user_id: current_user.id, name: params[:name], price: params[:price],
+                                   quantity: params[:quantity], measurement_unit: params[:measurement_unit])
     if @food.save
       redirect_to(user_foods_path(user_id: @user.id), notice: 'Food was successfully created.')
     else
