b70ceaa8d0431d465851baa7ff53956f1c416a96
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-08-31 14:55:06 +0500

    Add total food items in the recipe cards

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :user
-  has_many :recipes_foods, dependent: :destroy, class_name: 'RecipeFood'
+  has_many :recipe_foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy, class_name: 'RecipeFood', through: :recipe_foods
 
   validates :name, :price, :quantity, :measurement_unit, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :recipes_foods, dependent: :destroy, class_name: 'RecipeFood'
+  has_many :recipe_foods, dependent: :destroy
+  has_many :foods, dependent: :destroy, class_name: 'RecipeFood', through: :recipe_foods
 
   validates :name, :preparation_time, :cooking_time, :description, presence: true
 end
