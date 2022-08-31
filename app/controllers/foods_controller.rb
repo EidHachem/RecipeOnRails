fc45eb632453112006d716138e0011be3c091ae6
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-08-31 09:48:58 +0500

    Arrange food list in alphabetical order

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,6 @@
 class FoodsController < ApplicationController
   def index
-    @foods = Food.where(user_id: current_user.id)
+    @foods = Food.where(user_id: current_user.id).order(:name)
   end
 
   def show
